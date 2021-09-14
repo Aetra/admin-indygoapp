6b7c5937b4efb2126c16a75b2fda779807cbf586
Aetra <valentin.hajek1@etu.univ-lorraine.fr>
2021-09-14 18:22:25 +0200

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+# base image
+FROM node:12.13-alpine as builder
+
+WORKDIR /usr/src/app
+COPY package.json /usr/src/app
+RUN npm install --loglevel warn
+COPY . .
+RUN npm run build
+
+FROM nginx:latest
+USER root
+COPY --from=builder /usr/src/app/dist/indygo /usr/share/nginx/html
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,15 @@
+export const environment = {
+  production: true,
+  backend: {
+    apiurl: 'https://api.bde-indygo.fr/',
+    endpoints: {
+      login: 'login',
+      logout: 'logout',
+      contact: 'contact',
+      users: 'users',
+      signup: 'signup',
+      commentaire: 'commentaire',
+      messagesite: 'messageAccueil',
+    }
+  }
+};
